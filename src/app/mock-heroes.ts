028d37521d1b7ab657f201253f70fb74b66cd9b4
naveenraju <naveenraju@hakunamatata.in>
2018-11-30 10:00:37 +0530

    Angular 6 initial commit upto routing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'naveen'},
+    {id: 2, name: 'raju'},
+    {id: 3, name: 'm'},
+    {id: 4, name: 'chowdary'}
+]
